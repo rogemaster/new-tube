66d8e4fdc8df824477c92d9058d9d7173f7ee196
rogemaster <rlaaudwn000@naver.com>
2025-07-04 11:05:01 +0900

    에러 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
-  "/protected(*)",
-])
+  "/protected(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
